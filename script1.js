6e8e3d46c102f95986856786c16ee5b051ab63c8
manav37 <106614820+manav37@users.noreply.github.com>
2022-06-11 16:04:07 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,154 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3") /*background music*/
+let audioTurn = new Audio("ting.mp3") /*music when turn changes(X TO O or O to X)*/
+let gameover = new Audio("win.mp3") /*music when game is over*/
+let turn = "X"/*initially turn is of X*/
+let isgameover = false;
+let c=0;
+// Function to change the turn
+const changeTurn = ()=>{ 
+    return turn === "X"? "0": "X"
+}
+
+// Function to check for a win
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext'); /*get all boxtext*/
+    let wins = [  /*all possibilities of winning*/
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    for(let i=0;i<8;i++){
+        let arr=wins[i];
+        if((boxtext[arr[0]].innerText===boxtext[arr[1]].innerText) && (boxtext[arr[1]].innerText===boxtext[arr[2]].innerText) && boxtext[arr[0]].innerText!==""){
+            document.querySelector('.info').innerText = boxtext[arr[0]].innerText + " Won"
+            isgameover = true
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            if(arr[0]===0 && arr[1]===1 && arr[2]===2){
+                
+                document.getElementById("strike").style="";
+
+                document.getElementById("strike").className="strike-row-1";
+                gameover.play();
+                break;
+               
+
+            }
+
+            else if(arr[0]===3 && arr[1]===4 && arr[2]===5){
+                
+                document.getElementById("strike").style="";
+                document.getElementById("strike").className="strike-row-2";
+
+                gameover.play();
+                break;
+
+                
+            }
+            else if(arr[0]===6 && arr[1]===7 && arr[2]===8){
+                
+                document.getElementById("strike").style="";
+                document.getElementById("strike").className="strike-row-3";
+                gameover.play();
+                break;
+                
+            }
+            else if(arr[0]===0 && arr[1]===3 && arr[2]===6){
+                
+                document.getElementById("strike").style="";
+                document.getElementById("strike").className="strike-column-1";
+                gameover.play();
+                break;
+                
+            }
+            else if(arr[0]===1 && arr[1]===4 && arr[2]===7){
+                
+                document.getElementById("strike").style="";
+                document.getElementById("strike").className="strike-column-2";
+                gameover.play();
+                break;
+                
+            }
+            else if(arr[0]===2 && arr[1]===5 && arr[2]===8){
+                
+                document.getElementById("strike").style="";
+                document.getElementById("strike").className="strike-column-3";
+                gameover.play();
+                break;
+            }
+            else if(arr[0]===0 && arr[1]===4 && arr[2]===8){
+                
+                document.getElementById("strike").style="";
+                document.getElementById("strike").className="strike-diagonal-1";
+                gameover.play();
+                break;
+               
+            }
+            else if(arr[0]===2 && arr[1]===4 && arr[2]===6){
+                document.getElementById("strike").style="";
+                
+                document.getElementById("strike").className="strike-diagonal-2";
+                gameover.play();
+                break;
+                
+            }
+        }
+    }
+    if(isgameover){
+        
+        return;
+    }
+}
+
+// Game Logic
+// music.play()
+let boxes = document.getElementsByClassName("box");  /*we r adding click listener to each box*/
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext'); /*to get the span element whose content we have to change*/
+    element.addEventListener('click', ()=>{ /*we have to apply event listener on div not on span element bcoz boxtext is empty so there is no span element right now span is visible when we press and X or 0 appears, so element.eventListener hai not boxtext.eventListener*/
+        if(isgameover){
+            return;
+        }
+        if(boxtext.innerText === ''){ /*innerText returns the text content of the element, if it is empty means we have to add turn(X OR 0)*/ 
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+
+            if (!isgameover){
+                document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn; /*info class contains whose turn it is*/
+            }
+            else{
+                
+                return;
+            }
+
+        }
+    })
+    
+})
+
+
+
+
+// Add onclick listener to reset button
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext'); /*on clicking reset we want content of all boxtext(X AND 0) to become empty*/
+    Array.from(boxtexts).forEach(element => {
+        element.innerText = ""
+    });
+    turn = "X"; /*turn gets reset to initial value(X)*/
+    isgameover = false /*game will start again*/
+    
+    document.getElementsByClassName("info")[0].innerText  = "Turn for " + turn; /*Turn for X will be displayed again*/
+    document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "0px" /*image will disappear again*/
+    document.getElementById('strike').style.height="0";
+    document.getElementById('strike').style.width="0";
+    //document.getElementById('strike').style="";
+})
+
